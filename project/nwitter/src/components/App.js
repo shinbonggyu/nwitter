e17e1945f3d25f7e47c133da429cdfa5ac4bd9e4
shinbonggyu <bgshin96@naver.com>
2022-09-25 18:40:02 +0900

    09.25

---
+++ project/nwitter/src/components/App.js
@@ -0,0 +1,13 @@
+import {useState} from "react";
+import AppRouter from "./Router";
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  return (
+  <>
+  <AppRouter isLoggedIn ={isLoggedIn}/>
+  <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+  </>
+  );
+}
+
+export default App;
